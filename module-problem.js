b45ca3d209fcfcfd0a90640af65fb8b073262c6f
TFMahabub <rjmahabub543@gmail.com>
2022-09-09 15:47:55 +0600

    problem-3

--- module-problem.js
+++ module-problem.js
@@ -1,9 +1,33 @@
 // 2-------------------------------------------
-let storeFalsyValue = true;
+// let storeFalsyValue = true;
 
-if(!storeFalsyValue){
-  console.log('Nothing to show');
+// if(!storeFalsyValue){
+//   console.log('Nothing to show');
+// }
+// else{
+//   console.log('You are amazing');
+// }
+
+// 3-----------------------------------------------
+function add(a, b){
+  return a+b;
 }
-else{
-  console.log('You are amazing');
+function subtract(a, b){
+  return a-b;
 }
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+function modulus(a,b){
+  return a%b;
+}
+
+const getResult = (para1, para2, callFunction) =>{
+  const result = callFunction(para1,para2);
+  console.log(result);
+}
+
+getResult(60, 30, divide)
